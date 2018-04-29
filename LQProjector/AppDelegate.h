16ad4bc811eee5eebcd83d1ef66c57a0ca51ebc7
LiuQiqiang <lqq200912408@163.com>
2018-04-29 21:46:22 +0800

    Initial Commit

---
+++ LQProjector/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LQProjector
+//
+//  Created by LiuQiqiang on 2018/4/29.
+//  Copyright © 2018年 LiuQiqiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LQProjectorTests/LQProjectorTests.m
@@ -0,0 +1,39 @@
+//
+//  LQProjectorTests.m
+//  LQProjectorTests
+//
+//  Created by LiuQiqiang on 2018/4/29.
+//  Copyright © 2018年 LiuQiqiang. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface LQProjectorTests : XCTestCase
+
+@end
+
+@implementation LQProjectorTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
